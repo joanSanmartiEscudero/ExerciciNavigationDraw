1c34a2f8cb56669b0d580e1fbef0e978e746b609
Joan Sanmartí Escudero <joan.sanmarti@enti.cat>
2019-04-24 00:00:24 +0200

    EM CAGO EN GOOGLE APIS

--- NavigationDraw/app/src/main/java/com/example/carles/navigationdraw/MapsActivity.java
+++ NavigationDraw/app/src/main/java/com/example/carles/navigationdraw/MapsActivity.java
@@ -19,7 +19,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and get notified when the mapa is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -27,8 +27,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
+     * Manipulates the mapa once available.
+     * This callback is triggered when the mapa is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
